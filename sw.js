3831c81a1f2531fc84a9d6f716b5fd9dc8a72c4d
AlphaKiir <123605609+AlphaKiir@users.noreply.github.com>
2026-07-01 19:07:05 +0200

    Bump SW v10

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// KILL-SWITCH SW v9 (01/07 19h) : vide les caches, se desinscrit, recharge les clients.
+// KILL-SWITCH SW v10 (01/07 20h) : vide les caches, se desinscrit, recharge les clients.
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',(e)=>{e.waitUntil((async()=>{try{const keys=await caches.keys();await Promise.all(keys.map((k)=>caches.delete(k)));await self.registration.unregister();const cs=await self.clients.matchAll({type:'window'});cs.forEach((c)=>c.navigate(c.url));}catch(err){}})());});
