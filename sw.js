85dd7046ff3911a5c677bc339594eae4c7e4c916
AlphaKiir <123605609+AlphaKiir@users.noreply.github.com>
2026-07-03 12:08:43 +0200

    Bump SW v13

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// KILL-SWITCH SW v12 (02/07) : vide caches, se desinscrit, recharge clients.
+// KILL-SWITCH SW v13 (02/07) : vide caches, se desinscrit, recharge clients.
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',(e)=>{e.waitUntil((async()=>{try{const keys=await caches.keys();await Promise.all(keys.map((k)=>caches.delete(k)));await self.registration.unregister();const cs=await self.clients.matchAll({type:'window'});cs.forEach((c)=>c.navigate(c.url));}catch(err){}})());});
