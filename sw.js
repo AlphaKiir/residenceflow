141fe6eceaa56aee2255492d3d5bd4c32f714100
AlphaKiir <123605609+AlphaKiir@users.noreply.github.com>
2026-07-06 16:25:23 +0200

    Bump SW v19

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// KILL-SWITCH SW v18
+// KILL-SWITCH SW v19
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',(e)=>{e.waitUntil((async()=>{try{const keys=await caches.keys();await Promise.all(keys.map((k)=>caches.delete(k)));await self.registration.unregister();const cs=await self.clients.matchAll({type:'window'});cs.forEach((c)=>c.navigate(c.url));}catch(err){}})());});
