623b8aaa5e42e0e485478b3abc826e735d05b112
AlphaKiir <123605609+AlphaKiir@users.noreply.github.com>
2026-07-07 15:30:30 +0200

    SW v29

--- sw.js
+++ sw.js
@@ -1,3 +1,3 @@
-// SW v28
+// SW v29
 self.addEventListener('install',()=>self.skipWaiting());
 self.addEventListener('activate',(e)=>{e.waitUntil((async()=>{try{const k=await caches.keys();await Promise.all(k.map(x=>caches.delete(x)));await self.registration.unregister();const c=await self.clients.matchAll({type:'window'});c.forEach(x=>x.navigate(x.url));}catch(err){}})());});
